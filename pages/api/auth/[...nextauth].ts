a083aaedd5b3124e1afb70830a3dc5140c40a70b
Hein Ko Zin <ko.zin@viatick.com>
2022-11-25 09:07:26 +0630

    ✨feature: auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,147 @@
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+import axios from "axios";
+
+export interface UserLoginSuccessInterface {
+	success: boolean;
+	token: string;
+	message: string;
+	data: Data;
+}
+
+export interface Data {
+	id: number;
+	fullname: string;
+	email: string;
+	email_verified_at: any;
+	phone_no: any;
+	is_admin: any;
+	is_active: any;
+	last_login: any;
+	role: any;
+	status: any;
+	dob: any;
+	gender: any;
+	profile_img: any;
+	provider: any;
+	provider_id: any;
+	provider_token: any;
+	created_at: string;
+	updated_at: string;
+}
+
+export interface UserLoginFailedInterface {
+	success: boolean;
+	message: string;
+}
+
+export const authOptions: NextAuthOptions = {
+	providers: [
+		CredentialProvider({
+			name: "credentials",
+			credentials: {
+				email: { label: "Email", type: "email" },
+				password: { label: "Password" },
+			},
+
+			authorize: async (credentials) => {
+				const data = (await fetch(`${process.env.API_URL}auth/login`, {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(credentials),
+				}).then((res) => res.json())) as
+					| UserLoginSuccessInterface
+					| UserLoginFailedInterface;
+
+				console.log(data);
+
+				if ("token" in data) {
+					return {
+						id: data.data.id.toString(),
+						token: data.token,
+						user: {
+							id: data.data.id,
+							name: data.data.fullname,
+							email: data.data.email,
+						},
+					};
+				}
+
+				// if (data. === "Yes") {
+				// 	return {
+				// 		token: data.token,
+				// 		user: {
+				// 			id: data.data.user.id,
+				// 			user_type: data.data.user_type,
+				// 			name: data.data.user.employee.name,
+				// 			email: data.data.user.employee.email,
+				// 			image: data.data.user.employee.img,
+				// 			role: {
+				// 				department: data.data.departmentInfo[0].name,
+				// 				position: data.data.positionInfo[0].name,
+				// 			},
+				// 		},
+				// 	};
+				// }
+				return null;
+			},
+		}),
+	],
+
+	callbacks: {
+		jwt: async ({ token, user }) => {
+			if (user) {
+				token.token = user.token;
+				token.user = user.user;
+			}
+			const data = await axios
+				.get(`${process.env.API_URL}user`, {
+					headers: {
+						Authorization: `${token.token}`,
+					},
+				})
+				.then((res) => res.data);
+
+			token.user = {
+				id: data.data.employee.id,
+				name: data.data.employee.name,
+				email: data.data.employee.email,
+				image: data.data.employee.img,
+			};
+
+			return token;
+		},
+		session: async ({ session, token, user }) => {
+			if (token) {
+				session.token = token.token;
+				session.user = token.user;
+			}
+
+			return session;
+		},
+		async redirect({ url, baseUrl }) {
+			// Allows relative callback URLs
+			if (url.startsWith("/")) return `${baseUrl}${url}`;
+			// Allows callback URLs on the same origin
+			else if (new URL(url).origin === baseUrl) return url;
+			return baseUrl;
+		},
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+	jwt: {
+		secret: process.env.NEXTAUTH_SECRET,
+		maxAge: 60 * 60 * 24 * 30,
+	},
+
+	session: {
+		strategy: "jwt",
+		maxAge: 60 * 60 * 24 * 30,
+	},
+
+	pages: {
+		signIn: "/auth/login",
+	},
+};
+
+export default NextAuth(authOptions);
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,20 +1,20 @@
 import MainLayout from "@layouts/MainLayout";
 import { VisibilityOff, Visibility } from "@mui/icons-material";
 import {
-  Box,
-  Stack,
-  Typography,
-  colors,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Grid,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Button,
-  useMediaQuery,
-  useTheme,
+	Box,
+	Stack,
+	Typography,
+	colors,
+	IconButton,
+	InputAdornment,
+	TextField,
+	Grid,
+	Checkbox,
+	FormControlLabel,
+	FormGroup,
+	Button,
+	useMediaQuery,
+	useTheme,
 } from "@mui/material";
 import Head from "next/head";
 import React from "react";
@@ -23,166 +23,179 @@ import { BsFacebook } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = React.useState(false);
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
+	const [showPassword, setShowPassword] = React.useState(false);
+	const handleClickShowPassword = () => {
+		setShowPassword(!showPassword);
+	};
+	const handleMouseDownPassword = (
+		event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		event.preventDefault();
+	};
 
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  return (
-    <Box>
-      <Head>
-        <title>Loi Heng | Login</title>
-        <meta name="description" content="Loi Heng Login" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Stack
-        justifyContent={"center"}
-        direction={"row"}
-        alignItems={"center"}
-        sx={{ py: 6 }}
-      >
-        <Box
-          sx={{
-            px: 4,
-            py: 4,
-            border: `1px solid ${colors.grey[300]}`,
-            borderRadius: "4px",
-          }}
-        >
-          <Box
-            minWidth={isMobile ? 400 : 500}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Box>
-              <img src="/logo-only.png" alt="" />
-            </Box>
-            <Typography variant="h5">Loi Heng International</Typography>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 4,
-                width: "100%",
-                mt: 4,
-              }}
-            >
-              <TextField
-                type={"text"}
-                id="outlined-basic"
-                label="Email or Phone"
-                variant="outlined"
-              />
-              <TextField
-                type={showPassword ? "text" : "password"}
-                label="Password"
-                id="fullWidth"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Box>
-              <FormGroup>
-                <FormControlLabel
-                  control={<Checkbox defaultChecked />}
-                  label="Remember me"
-                />
-              </FormGroup>
-            </Box>
-            <Box>
-              <Typography>Forgot password?</Typography>
-            </Box>
-          </Box>
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Box sx={{ width: "300px" }}>
-              <Button variant={"contained"} fullWidth>
-                Login
-              </Button>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              my: 1,
-              "&::before": {
-                content: '""',
-                px: 1,
-                width: "50%",
-                height: "1px",
-                backgroundColor: "rgba(0, 0, 0, 0.15)",
-              },
-              "&::after": {
-                content: '""',
-                width: "50%",
-                height: "1px",
-                backgroundColor: "rgba(0, 0, 0, 0.15)",
-              },
-            }}
-          >
-            <span style={{ margin: "0px 12px" }}>or</span>
-          </Box>
-          <Box sx={{ display: "flex", justifyContent: "center", gap: 4 }}>
-            <IconButton>
-              <BsFacebook style={{ color: "#15A2FA" }} />
-            </IconButton>
-            <IconButton>
-              <FcGoogle />
-            </IconButton>
-            <IconButton>
-              <AiFillTwitterCircle style={{ color: "#1DA1F2" }} />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}>
-            <Box sx={{ display: "flex", gap: 1 }}>
-              <Typography>I have not registered yet?</Typography>
-              <Link href={"/auth/register"} legacyBehavior>
-                <a style={{ color: colors.blue[600], fontWeight: 500 }}>
-                  Sign Up
-                </a>
-              </Link>
-            </Box>
-          </Box>
-        </Box>
-      </Stack>
-    </Box>
-  );
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+	return (
+		<Box>
+			<Head>
+				<title>Loi Heng | Login</title>
+				<meta name="description" content="Loi Heng Login" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Stack
+				justifyContent={"center"}
+				direction={"row"}
+				alignItems={"center"}
+				sx={{ py: 6 }}
+			>
+				<Box
+					sx={{
+						px: 4,
+						py: 4,
+						border: `1px solid ${colors.grey[300]}`,
+						borderRadius: "4px",
+					}}
+				>
+					<Box
+						minWidth={isMobile ? 400 : 500}
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+						}}
+					>
+						<Box>
+							<img src="/logo-only.png" alt="" />
+						</Box>
+						<Typography variant="h5">Loi Heng International</Typography>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								gap: 4,
+								width: "100%",
+								mt: 4,
+							}}
+						>
+							<TextField
+								type={"text"}
+								id="outlined-basic"
+								label="Email or Phone"
+								variant="outlined"
+							/>
+							<TextField
+								type={showPassword ? "text" : "password"}
+								label="Password"
+								id="fullWidth"
+								InputProps={{
+									endAdornment: (
+										<InputAdornment position="end">
+											<IconButton
+												aria-label="toggle password visibility"
+												onClick={handleClickShowPassword}
+												onMouseDown={handleMouseDownPassword}
+												edge="end"
+											>
+												{showPassword ? <VisibilityOff /> : <Visibility />}
+											</IconButton>
+										</InputAdornment>
+									),
+								}}
+							/>
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							alignItems: "center",
+						}}
+					>
+						<Box>
+							<FormGroup>
+								<FormControlLabel
+									control={<Checkbox defaultChecked />}
+									label="Remember me"
+								/>
+							</FormGroup>
+						</Box>
+						<Box>
+							<Typography>Forgot password?</Typography>
+						</Box>
+					</Box>
+					<Box sx={{ display: "flex", justifyContent: "center" }}>
+						<Box sx={{ width: "300px" }}>
+							<Button
+								variant={"contained"}
+								onClick={() => {
+									signIn("credentials", {
+										email: "heinkozin@gmail.com",
+										password: "password",
+										redirect: false,
+									}).then((data) => {
+										console.log(data);
+									});
+								}}
+								fullWidth
+							>
+								Login
+							</Button>
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							my: 1,
+							"&::before": {
+								content: '""',
+								px: 1,
+								width: "50%",
+								height: "1px",
+								backgroundColor: "rgba(0, 0, 0, 0.15)",
+							},
+							"&::after": {
+								content: '""',
+								width: "50%",
+								height: "1px",
+								backgroundColor: "rgba(0, 0, 0, 0.15)",
+							},
+						}}
+					>
+						<span style={{ margin: "0px 12px" }}>or</span>
+					</Box>
+					<Box sx={{ display: "flex", justifyContent: "center", gap: 4 }}>
+						<IconButton>
+							<BsFacebook style={{ color: "#15A2FA" }} />
+						</IconButton>
+						<IconButton>
+							<FcGoogle />
+						</IconButton>
+						<IconButton>
+							<AiFillTwitterCircle style={{ color: "#1DA1F2" }} />
+						</IconButton>
+					</Box>
+					<Box sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}>
+						<Box sx={{ display: "flex", gap: 1 }}>
+							<Typography>I have not registered yet?</Typography>
+							<Link href={"/auth/register"} legacyBehavior>
+								<a style={{ color: colors.blue[600], fontWeight: 500 }}>
+									Sign Up
+								</a>
+							</Link>
+						</Box>
+					</Box>
+				</Box>
+			</Stack>
+		</Box>
+	);
 };
 
 Login.getLayout = function getLayout(page: ReactElement) {
-  return <MainLayout>{page}</MainLayout>;
+	return <MainLayout>{page}</MainLayout>;
 };
 
 export default Login;
