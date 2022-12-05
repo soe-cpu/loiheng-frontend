9090742a2fdacd3ab14b6ad3203198fa0f65222a
soe-cpu <soelinaung709@gmail.com>
2022-12-05 11:15:12 +0630

    home page banner conflit fixed

--- src/utils/apis/useAllHomePageBanner.ts
+++ src/utils/apis/useAllHomePageBanner.ts
@@ -2,10 +2,10 @@ import { GetHomePageBannerListResponse } from "@atoms/homePageBannerListAtom";
 import useSWR from "swr";
 import fetcher from "../fetcher";
 
-const useAllHomePageBanner = () => {
+const useAllHomePageBanner = (token?: string) => {
   const url = process.env.API_URL;
   const res = useSWR<GetHomePageBannerListResponse>(
-    [`${url}home-page-banner-image`],
+    [`${url}home-page-banner-image`, token],
     fetcher
   );
   return res;
