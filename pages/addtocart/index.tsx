37e7798492244f8e349dea12d0668de084e75a67
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-13 16:56:50 +0630

    ✨feature: bind address

--- pages/addtocart/index.tsx
+++ pages/addtocart/index.tsx
@@ -1,24 +1,57 @@
 import { AddToCartComponent } from "@components/AddToCartComponent";
 import MainLayout from "@layouts/MainLayout";
 import { Box, Container, Typography } from "@mui/material";
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth/next";
 import Head from "next/head";
+import { authOptions, Me } from "pages/api/auth/[...nextauth]";
 import { ReactElement } from "react";
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await unstable_getServerSession(
+		context.req,
+		context.res,
+		authOptions
+	);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/auth/login",
+				permanent: false,
+			},
+		};
+	}
+
+	const data = (await axios
+		.get(`${process.env.API_URL}auth/me`, {
+			headers: {
+				Authorization: `${session.token}`,
+			},
+		})
+		.then((res) => res.data)) as Me;
+
+	return {
+		props: { data },
+	};
+};
+
 const AddToCart = () => {
-  return (
-    <Box>
-      <Head>
-        <title>Loi Heng | AddToCart</title>
-        <meta name="description" content="Loi Heng Add To Cart" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <AddToCartComponent />
-    </Box>
-  );
+	return (
+		<Box>
+			<Head>
+				<title>Loi Heng | AddToCart</title>
+				<meta name="description" content="Loi Heng Add To Cart" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<AddToCartComponent />
+		</Box>
+	);
 };
 
 AddToCart.getLayout = function getLayout(page: ReactElement) {
-  return <MainLayout>{page}</MainLayout>;
+	return <MainLayout>{page}</MainLayout>;
 };
 
 export default AddToCart;
---
+++ src/components/ProfileComponent/AddressCard.tsx
@@ -0,0 +1,94 @@
+import {
+	Box,
+	colors,
+	Typography,
+	Divider,
+	Stack,
+	Tooltip,
+	IconButton,
+	Grid,
+	styled,
+} from "@mui/material";
+import { Address } from "@stores/addressStore";
+import React from "react";
+import { BiEdit } from "react-icons/bi";
+import { ImBin } from "react-icons/im";
+
+export const AddressCard = (props: Address) => {
+	const add = props;
+	return (
+		<Grid item xs={12} md={6} lg={4}>
+			<Box sx={{ border: `1px solid ${colors.grey[300]}` }}>
+				<Typography sx={{ p: 2 }}>{add?.full_name}</Typography>
+				<Divider />
+				<Box
+					sx={{
+						p: 2,
+						display: "flex",
+						flexDirection: "column",
+						gap: 1,
+					}}
+				>
+					<Typography variant="subtitle2">
+						Name: <StyledSpan>{add.full_name}</StyledSpan>
+					</Typography>
+					<Typography variant="subtitle2">
+						Address Type: <StyledSpan>home</StyledSpan>
+					</Typography>
+					<Typography variant="subtitle2">
+						City: <StyledSpan>{add.city}</StyledSpan>
+					</Typography>
+					<Typography variant="subtitle2">
+						Township: <StyledSpan>{add.township}</StyledSpan>
+					</Typography>
+					<Typography variant="subtitle2">
+						Region: <StyledSpan>{add.region}</StyledSpan>
+					</Typography>
+					<Typography variant="subtitle2">
+						Phone Number: <StyledSpan>{add.phone}</StyledSpan>
+					</Typography>
+				</Box>
+				<Divider />
+				<Box sx={{ p: 2, display: "flex", justifyContent: "end" }}>
+					<Stack direction={"row"} spacing={1}>
+						<Tooltip title={"Edit Address"} arrow placement="top">
+							<IconButton
+								sx={{
+									border: `1px solid ${colors.blue[600]}`,
+									color: colors.blue[600],
+									transition: "0.4s",
+									"&:hover": {
+										backgroundColor: colors.blue[600],
+										color: "#fff",
+									},
+								}}
+							>
+								<BiEdit style={{ fontSize: 16 }} />
+							</IconButton>
+						</Tooltip>
+						<Tooltip title={"Delete Address"} arrow placement="top">
+							<IconButton
+								sx={{
+									border: `1px solid ${colors.red[600]}`,
+									color: colors.red[600],
+									transition: "0.4s",
+									"&:hover": {
+										backgroundColor: colors.red[600],
+										color: "#fff",
+									},
+								}}
+							>
+								<ImBin style={{ fontSize: 16 }} />
+							</IconButton>
+						</Tooltip>
+					</Stack>
+				</Box>
+			</Box>
+		</Grid>
+	);
+};
+
+const StyledSpan = styled("span")(({ theme }) => ({
+	fontSize: 14,
+	color: colors.grey[500],
+}));
