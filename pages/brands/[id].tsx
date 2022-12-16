6740de4c8d20ad5d2d12b1ef8518026fd709a192
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-16 16:34:52 +0630

    🐞 fix: type for production

--- pages/brands/[id].tsx
+++ pages/brands/[id].tsx
@@ -2,6 +2,7 @@ import ProductByBrandComponent from "@components/BrandComponent/ProductByBrandCo
 import ProductByBrand from "@components/BrandComponent/ProductByBrandComponent";
 import MainLayout from "@layouts/MainLayout";
 import { Box, Container, Typography } from "@mui/material";
+import { Pagination, Product } from "@stores/wishlist.store";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { ReactElement } from "react";
@@ -16,110 +17,6 @@ export interface ProductByBrand {
 export interface Data {
 	products: Product[];
 	pagination: Pagination;
-}
-
-export interface Product {
-	id: number;
-	product_code: string;
-	name: string;
-	price: string;
-	cover_img: string;
-	description: any;
-	short_description: any;
-	sku: string;
-	stock: any;
-	desc_file: string;
-	approved_by: any;
-	approved_when: any;
-	status: any;
-	is_active: number;
-	is_preorder: any;
-	is_feature_product: number;
-	is_new_arrival: number;
-	created_by: CreatedBy[];
-	category: Category[];
-	brand: Brand[];
-	product_specs: ProductSpec[];
-	product_warranties: ProductWarranty[];
-	product_pictures: any[];
-	created_at: string;
-	updated_at: string;
-}
-
-export interface CreatedBy {
-	id: number;
-	fullname: string;
-	email: string;
-	email_verified_at: any;
-	phone_no: any;
-	is_admin: string;
-	is_active: number;
-	last_login: any;
-	role: any;
-	status: any;
-	dob: any;
-	gender: any;
-	profile_img: any;
-	provider: any;
-	provider_id: any;
-	provider_token: any;
-	created_at: string;
-	updated_at: string;
-}
-
-export interface Category {
-	id: number;
-	name: string;
-	description: string;
-	level: any;
-	picture: string;
-	picture_blob: any;
-	status: any;
-	created_by: number;
-	parent: number;
-	created_at: string;
-	updated_at: string;
-}
-
-export interface Brand {
-	id: number;
-	name: string;
-	description: string;
-	picture: string;
-	is_active: number;
-	created_by: number;
-	created_at: string;
-	updated_at: string;
-}
-
-export interface ProductSpec {
-	id: number;
-	product_id: number;
-	spec_key: string;
-	spec_value: string;
-	created_at: string;
-	updated_at: string;
-}
-
-export interface ProductWarranty {
-	id: number;
-	product_id: number;
-	service_key: string;
-	service_value: string;
-	created_at: string;
-	updated_at: string;
-}
-
-export interface Pagination {
-	current_page: number;
-	first_page_url: string;
-	last_page: number;
-	last_page_url: string;
-	next_page_url: any;
-	path: string;
-	per_page: number;
-	prev_page_url: any;
-	total: number;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
