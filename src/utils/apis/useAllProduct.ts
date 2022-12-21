d8d12986bee2616fd7a1d4048a79e92dc38a8fd3
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-21 13:17:38 +0630

    ✨feature: product filter has been bind

--- src/utils/apis/useAllProduct.ts
+++ src/utils/apis/useAllProduct.ts
@@ -2,10 +2,17 @@ import { GetProductListResponse } from "@atoms/productListAtom";
 import useSWR from "swr";
 import fetcher from "../fetcher";
 
-const useAllProduct = () => {
-  const url = process.env.API_URL;
-  const res = useSWR<GetProductListResponse>([`${url}products`], fetcher);
-  return res;
+const useAllProduct = (categories: number[], brands: number[]) => {
+	const url = process.env.API_URL;
+	const res = useSWR<GetProductListResponse>(
+		[
+			`${url}products?category_id=${categories.join(
+				","
+			)}&brand_id=${brands.join(",")}`,
+		],
+		fetcher
+	);
+	return res;
 };
 
 export default useAllProduct;
