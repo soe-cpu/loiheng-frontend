4e9ab26ed98eaf78b70b28e956ce5dc4be9e4d3d
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-26 19:46:31 +0630

    ✨feature: added some features

--- src/utils/apis/useAllNewArrivalProduct.ts
+++ src/utils/apis/useAllNewArrivalProduct.ts
@@ -2,13 +2,13 @@ import { GetProductListResponse } from "@atoms/productListAtom";
 import useSWR from "swr";
 import fetcher from "../fetcher";
 
-const useAllNewArrivalProduct = () => {
-  const url = process.env.API_URL;
-  const res = useSWR<GetProductListResponse>(
-    [`${url}products/new-arrivals`],
-    fetcher
-  );
-  return res;
+const useAllNewArrivalProduct = (page = 1) => {
+	const url = process.env.API_URL;
+	const res = useSWR<GetProductListResponse>(
+		[`${url}products/new-arrivals?page=${page}`],
+		fetcher
+	);
+	return res;
 };
 
 export default useAllNewArrivalProduct;
