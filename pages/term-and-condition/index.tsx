afd43a3371b80b8b795b190ee3d5c37fbdd70e64
soe-cpu <soelinaung709@gmail.com>
2023-01-11 15:24:27 +0630

    term and conditiion fixed

--- pages/term-and-condition/index.tsx
+++ pages/term-and-condition/index.tsx
@@ -8,38 +8,38 @@ import React from "react";
 import { GetSettingResponse } from "@atoms/settingListAtom";
 
 const TermAndCondition = () => {
-	const { data, error, isValidating } = useAllSetting("term_and_condition");
-	const [term, setTerm] = React.useState<GetSettingResponse>();
+  const { data, error, isValidating } = useAllSetting("term_and_condition");
+  const [term, setTerm] = React.useState<GetSettingResponse>();
 
-	React.useEffect(() => {
-		if (data) {
-			setTerm(data);
-		}
-	}, [data, setTerm]);
-	return (
-		<Box>
-			<Head>
-				<title>Loi Heng | Term and Condition</title>
-				<meta name="description" content="Loi Heng About Us" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Box sx={{ py: 2 }}>
-				<Container>
-					<Typography variant="h5">Term and Condition</Typography>
-					{JSON.stringify(term?.data.value)}
-					{/* {term ? (
+  React.useEffect(() => {
+    if (data) {
+      setTerm(data);
+    }
+  }, [data, setTerm]);
+  return (
+    <Box>
+      <Head>
+        <title>Loi Heng | Term and Condition</title>
+        <meta name="description" content="Loi Heng About Us" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box sx={{ py: 2 }}>
+        <Container>
+          <Typography variant="h5">Term and Condition</Typography>
+          {/* {JSON.stringify(term?.data.value)} */}
+          {term ? (
             <div dangerouslySetInnerHTML={{ __html: term.data.value }}></div>
           ) : (
             ""
-          )} */}
-				</Container>
-			</Box>
-		</Box>
-	);
+          )}
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 TermAndCondition.getLayout = function getLayout(page: ReactElement) {
-	return <MainLayout>{page}</MainLayout>;
+  return <MainLayout>{page}</MainLayout>;
 };
 
 export default TermAndCondition;
