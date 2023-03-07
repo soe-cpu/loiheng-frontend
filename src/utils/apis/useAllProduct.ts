dab9d5a7a1c8245ddb67998507e8209f2d7fd106
Hein Ko Zin <heinkozin4@gmail.com>
2023-03-07 20:23:51 +0630

    ✨feature: sorting is added

--- src/utils/apis/useAllProduct.ts
+++ src/utils/apis/useAllProduct.ts
@@ -7,12 +7,13 @@ const useAllProduct = (
 	brands?: number[],
 	keyword = "",
 	page = 1,
-	limit = 15
+	limit = 15,
+	sort_by = ""
 ) => {
 	const url = process.env.API_URL;
 	const res = useSWR<GetProductListResponse>(
 		[
-			`${url}products?category_id=${categories}&brand_id=${brands}&limit=${limit}&page=${page}&search=${keyword}`,
+			`${url}products?category_id=${categories}&brand_id=${brands}&limit=${limit}&page=${page}&search=${keyword}&sort_by=${sort_by}`,
 		],
 		fetcher
 	);
